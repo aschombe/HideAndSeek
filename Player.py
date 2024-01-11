f203c3893916b1319c377052b3adbd06e132f682
aschombe <aschombe@stevens.edu>
2024-01-11 14:15:51 -0500

    Updated README.md, added player class

---
+++ Player.py
@@ -0,0 +1,7 @@
+# Players
+class Player:
+    def __init__(self, color, position, radius, vision_cone):
+        self.color = color
+        self.position = position
+        self.radius = radius
+        self.vision_cone = vision_cone
